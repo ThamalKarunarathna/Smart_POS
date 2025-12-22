13dd112d8fac28ddb4a5619572b623d0b73262b7
ThamalKarunarathna <thamalkarunarathna7@gmail.com>
2025-12-22 10:11:26 +0530

    create PO section Models

--- app/Models/PurchaseOrder.php
+++ app/Models/PurchaseOrder.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class PurchaseOrder extends Model
 {
-    //
+
+     protected $fillable = [
+        'po_no','supplier_name','po_date','status','created_by','approved_by','approved_at'
+    ];
+
+    public function items()
+    {
+        return $this->hasMany(PurchaseOrderItem::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class::class, 'created_by');
+    }
+
+    public function approver()
+    {
+        return $this->belongsTo(User::class, 'approved_by');
+    }
+
+    public function isLocked(): bool
+    {
+        return $this->status === 'approved';
+    }
 }
--- app/Models/PurchaseOrderItem.php
+++ app/Models/PurchaseOrderItem.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class PurchaseOrderItem extends Model
 {
-    //
+     protected $fillable = ['purchase_order_id','item_id','qty','rate'];
+
+    public function purchaseOrder()
+    {
+        return $this->belongsTo(PurchaseOrder::class);
+    }
+
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
 }
